7005d906555c304fdd8f4b1401ca0dc24f3b3dab
BrunoEstevamRJ <Bruno.Estevam@outlook.com.br>
2022-10-27 18:48:58 -0300

    passw

--- db.js
+++ db.js
@@ -1,21 +1,7 @@
-// const mongoose = require('mongoose');
-
-// //Configurando Mongoose
-// mongoose.Promise = global.Promise;
-// mongoose.connect('mongodb://localhost:27017', {
-//     userMongoClient: true
-// }).then(() => {
-//     console.log('MongoDB Conectado...')
-// })
-//     .catch(() => {
-//         console.log('Erro de conexão com mongoDB...')
-//     });
-
-
 const mongoose = require('mongoose');
 
 async function startDB() {
-    //await mongoose.connect('mongodb+srv://BrunoEstevam:R0ckst4r!@@cluster0.zctgfwj.mongodb.net/test');
+    
     await mongoose.connect('mongodb://localhost:27017/BlogDB');
 
 }
